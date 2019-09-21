ed0fdb203a2756b1190492adb6228d234bec649a
syiin <xbio.oo@gmail.com>
2019-09-21 22:12:25 +0800

    finished first draft client

--- assignments/assignment1/client_server/client-c.c
+++ assignments/assignment1/client_server/client-c.c
@@ -30,7 +30,8 @@ void *get_in_addr(struct sockaddr *sa)
  * Open socket and send message from stdin.
  * Return 0 on success, non-zero on failure
 */
-int client(char *server_ip, char *server_port) {
+int client(char *server_ip, char *server_port)
+{
   int sockfd, numbytes;
   char buf[SEND_BUFFER_SIZE];
   struct addrinfo hints, *servinfo, *p;
@@ -41,6 +42,55 @@ int client(char *server_ip, char *server_port) {
   hints.ai_family = AF_UNSPEC;
   hints.ai_socktype = SOCK_STREAM;
 
+  if ((rv = getaddrinfo(server_ip, server_port, &hints, &servinfo)) != 0)
+  {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    return 1;
+  }
+
+  for (p = servinfo; p != NULL; p->ai_next)
+  {
+    if ((sockfd = socket(p->ai_family,
+                         p->ai_socktype,
+                         p->ai_protoco)))
+    {
+      perror("client: socket");
+      continue;
+    }
+
+    if (sockfd = connect(sockfd, p->ai_addr, p->ai_addrlen) == -1)
+    {
+      close(sockfd);
+      perror("client: connect");
+      continue;
+    }
+    break;
+  }
+
+  if (p == NULL)
+  {
+    fprintf(stderr, "client failed to connect\n");
+    return 2;
+  }
+
+  inet_ntop(p->ai_family,
+            get_in_addr((struct sockaddr *)p->ai_addr),
+            s,
+            sizeof s);
+  printf("client: connecting %s\n", s);
+  freeaddrinfo(servinfo);
+
+  if ((numbytes = recv(sockfd, buf, MAXDATASIZE - 1, 0)) == -1)
+  {
+    perror('recv');
+    ;
+    exit(1);
+  }
+
+  buf[numbytes] = '\0';
+  printf("client recieved: %s \n", buf);
+  close(sockfd);
+
   return 0;
 }
 
@@ -48,11 +98,13 @@ int client(char *server_ip, char *server_port) {
  * main()
  * Parse command-line arguments and call client function
 */
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   char *server_ip;
   char *server_port;
 
-  if (argc != 3) {
+  if (argc != 3)
+  {
     fprintf(stderr, "Usage: ./client-c [server IP] [server port] < [message]\n");
     exit(EXIT_FAILURE);
   }
