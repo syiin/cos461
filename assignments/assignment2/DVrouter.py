1bbc7236d238e7544977391185345f374cf8966d
syiin <xbio.oo@gmail.com>
2019-10-01 23:50:15 +0800

    divined that ports are actually links

--- assignments/assignment2/DVrouter.py
+++ assignments/assignment2/DVrouter.py
@@ -17,28 +17,29 @@ class DVrouter(Router):
     def __init__(self, addr, heartbeatTime):
         """TODO: add your own class fields and initialization code here"""
         Router.__init__(self, addr)  # initialize superclass - don't remove
-
+        self.heartbeatTime = heartbeatTime
+        self.routing_table = {}
 
     def handlePacket(self, port, packet):
         """TODO: process incoming packet"""
-        pass
-
+        if packet.dstAddr != self.addr:
+            self.send(port, packet)
+        else:
+            if packet.kind == 1:
+                print("This is a TRACEROUTE packet")
 
     def handleNewLink(self, port, endpoint, cost):
         """TODO: handle new link"""
         pass
 
-
     def handleRemoveLink(self, port):
         """TODO: handle removed link"""
         pass
-
 
     def handleTime(self, timeMillisecs):
         """TODO: handle current time"""
         pass
 
-
     def debugString(self):
         """TODO: generate a string for debugging in network visualizer"""
-        return ""
+        return "{}".format([(self.links[o].e1, self.links[o].e2) for o in self.links])
