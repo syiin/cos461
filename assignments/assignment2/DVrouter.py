cc253808a6a8117fa03696ff3e8f9e719fec65fe
syiin <xbio.oo@gmail.com>
2019-10-04 23:06:49 +0800

    figured out distance table and wrote helper functions

--- assignments/assignment2/DVrouter.py
+++ assignments/assignment2/DVrouter.py
@@ -21,30 +21,76 @@ class DVrouter(Router):
         Router.__init__(self, addr)  # initialize superclass - don't remove
         self.heartbeatTime = heartbeatTime
         self.dist_table = {}
-        self.port_list = []
+
+        self.debug_list = []
 
     def handlePacket(self, port, packet):
         """TODO: process incoming packet"""
         pkt_link = self.links[port]
         link_src = pkt_link.e1 if pkt_link.e1 != self.addr else pkt_link.e2
 
+        if packet.kind == 1:
+            req_pkt = self.handle_traceroute_pkt(port, packet)
+        else:
+            req_pkt = self.handle_routing_pkt(port, packet)
+        self.debug_list.append(
+            {"pd": packet.dstAddr, "addr2port": self.addr2port()})
+
+        # self.debug_list.append(
+        #     {"PORT_IDX": port,
+        #      "PACKET KIND": packet.kind,
+        #      "PACKET CONTENT": req_pkt,
+        #      #  "LINK N NOT ME": link_src,
+        #      "PACKET SRC": packet.srcAddr,
+        #      "PACKET DST": packet.dstAddr,
+        #      "DIST TABLE": self.dist_table})
+
+    def handle_traceroute_pkt(self, port, packet):
+        req_pkt = packet.content
         if packet.dstAddr != self.addr:
-            if link_src in self.dist_table:
-                fastest_port = min(self.dist_table, key=self.dist_table.get)
-                self.send(fastest_port, packet)
+            if self.is_neighbour(packet.dstAddr):
+                port = self.addr2port()[packet.dstAddr]
+                self.send(port, packet)
             else:
-                self.dist_table[link_src] = 1
                 random_port = self.get_random_port(port)
+                my_table = {'src': self.addr, 'dist_table': self.dist_table}
+                packet.content = str(my_table)
                 self.send(random_port, packet)
         else:
             print(packet)
+        return req_pkt
 
-        self.port_list.append(
-            {"PORT_IDX": port,
-             "LINK N NOT ME": link_src,
-             "PACKET SRC": packet.srcAddr,
-             "PACKET DST": packet.dstAddr,
-             "DIST TABLE": self.dist_table})
+    def handle_routing_pkt(self, port, packet):
+        req_pkt = packet.content
+        if packet.dstAddr != self.addr:
+            random_port = self.get_random_port(port)
+            packet.content = {'src': self.addr, 'dist_table': self.dist_table}
+            self.send(random_port, packet)
+        else:
+            print(packet)
+        return req_pkt
+
+    def is_neighbour(self, addr):
+        return addr in self.get_neighbours()
+
+    def addr2port(self):
+        return {v: k for k, v in self.links.items()}
+
+    def get_neighbours(self):
+        neighbours = {}
+        for port, link in self.links.items():
+            neighbours[link.e1] = {}
+            neighbours[link.e1][link.e2] = link.l12
+            neighbours[link.e2] = {}
+            neighbours[link.e2][link.e1] = link.l21
+        return neighbours
+
+    def get_random_port(self, except_port):
+        tmp_dict = self.links
+        if len(tmp_dict) > 1:
+            tmp_dict.pop(except_port, None)
+        random_port = random.choice(list(tmp_dict.keys()))
+        return random_port
 
     def handleNewLink(self, port, endpoint, cost):
         """TODO: handle new link"""
@@ -60,12 +106,5 @@ class DVrouter(Router):
 
     def debugString(self):
         """TODO: generate a string for debugging in network visualizer"""
-        return "{}".format(self.port_list)
+        return "{}".format(self.debug_list)
         # return "{}".format([(self.links[o].e1, self.links[o].e2) for o in self.links])
-
-    def get_random_port(self, except_port):
-        tmp_dict = self.links
-        if len(tmp_dict) > 1:
-            tmp_dict.pop(except_port, None)
-        random_port = random.choice(list(tmp_dict.keys()))
-        return random_port
