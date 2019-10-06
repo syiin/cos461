15d3949298a78c8792e8ec26ebe1bf3b59b0ffa6
syiin <xbio.oo@gmail.com>
2019-10-06 23:48:10 +0800

    handled basic traceroute()

--- assignments/assignment2/DVrouter.py
+++ assignments/assignment2/DVrouter.py
@@ -28,13 +28,13 @@ class DVrouter(Router):
 
     def handlePacket(self, port, packet):
         """TODO: process incoming packet"""
-        if packet.isTraceroute():
-            self.handle_traceroute_pkt(port, packet)
+        if packet.isRouting():
+            self.handle_routing(port, packet)
         else:
-            self.handle_routing_pkt(port, packet)
+            self.handle_traceroute(port, packet)
         self.debug_list = self.dist_table
 
-    def handle_traceroute_pkt(self, port, packet):
+    def handle_routing(self, port, packet):
         p_dist_table = ast.literal_eval(packet.content)
         p_dist_table.pop(self.addr, None)
         p_dist_table.pop(packet.srcAddr, None)
@@ -51,11 +51,15 @@ class DVrouter(Router):
                 packet_row['distance'] += 1
                 self.dist_table[row] = packet_row
 
-    def handle_routing_pkt(self, port, packet):
+    def handle_traceroute(self, port, packet):
         if packet.dstAddr != self.addr:
-            pass
+            if packet.dstAddr in self.dist_table:
+                send_port = self.dist_table[packet.dstAddr]['port']
+                self.send(send_port, packet)
+            else:
+                self.dist_table[packet.srcAddr] = {'port': port, 'distance': 1}
         else:
-            print(packet)
+            self.try_send(packet)
 
     def handleNewLink(self, port, endpoint, cost):
         """TODO: handle new link"""
@@ -73,11 +77,14 @@ class DVrouter(Router):
             dstAddr = self.addr
             content = str(self.dist_table)
             routing_pkt = Packet(kind, srcAddr, dstAddr, content)
-            for port in range(4):
-                try:
-                    self.send(port, routing_pkt)
-                except:
-                    pass
+            self.try_send(routing_pkt)
+
+    def try_send(self, pkt):
+        for port in range(4):
+            try:
+                self.send(port, pkt)
+            except:
+                pass
 
     def debugString(self):
         """TODO: generate a string for debugging in network visualizer"""
